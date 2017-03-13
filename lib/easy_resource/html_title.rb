e3a49e454921aae171d28eadc73c701e60e0601a
Jan Uhlar <jan.uhlar@topmonks.com>
2017-03-13 14:26:43 +0100

    [FIX] html_title to be inherited

--- lib/easy_resource/html_title.rb
+++ lib/easy_resource/html_title.rb
@@ -7,20 +7,17 @@ module EasyResource
       include EasyResource::ProcOrObject
 
       helper_method :html_title
-      cattr_accessor :html_title
-
-      def html_title(key = action_name)
-        proc_object(self.class.html_title[key.to_sym], view_context)
-      end
+      class_attribute :html_title, instance_accessor: false, instance_predicate: false
     end
 
     module ClassMethods
       def prepare_html_title(options = {})
-        self.html_title ||= {}
-        options.each do |action, value|
-          self.html_title[action] = value
-        end
+        self.html_title = options
       end
+    end
+
+    def html_title(key = action_name)
+      proc_object(self.class.html_title[key.to_sym], view_context)
     end
   end
 end
